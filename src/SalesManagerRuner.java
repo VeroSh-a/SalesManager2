b73f18cc2ba988e544ff432c2ee8aca214e207c1
Veronika <VeronikaShatan@yandex.ru>
2023-01-28 01:11:47 +0300

    Added method middleResult

--- src/SalesManagerRuner.java
+++ src/SalesManagerRuner.java
@@ -2,10 +2,10 @@ import netology.SalesManager;
 
 public class SalesManagerRuner {
     public static void main(String[] args) {
-//        long result;
-//        long[] sales = {1, 3, 5, 7};
-        int result;
-        int[] sales = {1, 3, 5, 7};
+        long result;
+        long[] sales = {1, 3, 5, 7};
+        //int result;
+        //int[] sales = {1, 3, 5, 7};
         SalesManager SM = new SalesManager(sales);
         //result = SM.max();
         result = SM.middleResult();
