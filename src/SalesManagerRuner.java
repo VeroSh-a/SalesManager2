40b14dc6fbd666c3449624f388fb02c1702eefbe
Veronika <VeronikaShatan@yandex.ru>
2023-01-28 01:06:56 +0300

    Change type int-long

--- src/SalesManagerRuner.java
+++ src/SalesManagerRuner.java
@@ -2,13 +2,13 @@ import netology.SalesManager;
 
 public class SalesManagerRuner {
     public static void main(String[] args) {
-        long result;
-        long[] sales = {1, 3, 5, 7};
+//        long result;
+//        long[] sales = {1, 3, 5, 7};
+        int result;
+        int[] sales = {1, 3, 5, 7};
         SalesManager SM = new SalesManager(sales);
         //result = SM.max();
         result = SM.middleResult();
         System.out.println(result);
-
-
     }
 }
