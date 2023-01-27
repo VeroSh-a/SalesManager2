341856f6ff83f46d612ccf8c6a6d02ee20f2f1cc
Veronika <VeronikaShatan@yandex.ru>
2023-01-27 20:26:23 +0300

    Added method calculate of middle sales result

--- src/netology/SalesManager.java
+++ src/netology/SalesManager.java
@@ -16,25 +16,25 @@ public class SalesManager {
         }
         return max;
     }
-//    public int min(){
-//        int min =2147483647;
-//        for (int saleMin : sales){
-//            if (saleMin < min) {
-//                min = saleMin;
-//            }
-//        }
-//        return min;
-//    }
-//    public int middleResult(){
-//        // среднее арифметическое - сумма всех чисел, деленная на их количество
-//        int middleSales = 0;
-//        if (sales.length > 0) {
-//            int sum = 0;
-//            for (int j = 0; j < sales.length; j++) {
-//                sum += sales[j];
-//            }
-//            middleSales = ((sum - min()) - max())  / (sales.length - 2);
-//        }
-//        return middleSales;
-//    }
+    public int min(){
+        int min =2147483647;
+        for (int saleMin : sales){
+            if (saleMin < min) {
+                min = saleMin;
+            }
+        }
+        return min;
+    }
+    public int middleResult(){
+        // среднее арифметическое - сумма всех чисел, деленная на их количество
+        int middleSales = 0;
+        if (sales.length > 0) {
+            int sum = 0;
+            for (int j = 0; j < sales.length; j++) {
+                sum += sales[j];
+            }
+            middleSales = ((sum - min()) - max())  / (sales.length - 2);
+        }
+        return middleSales;
+    }
 }
